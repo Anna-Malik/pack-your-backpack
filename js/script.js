2720e0c679527ea96af8302449244aae990c9371
Anna Malik <malik.anna1992@gmail.com>
2022-04-11 10:40:49 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -1,24 +1,64 @@
 {
-    const tasks = [];
+    const tasks = [
+       
+    ];
 
+    const onFormSubmit = (event) => {
+       
+            event.preventDefault();
+
+            const newItemContent = document.querySelector(".js-newItem").value.trim();
+            
+            if (newItemContent === "") {
+                return;
+            } 
+        addNewItem(newItemContent);
+        
+    }
     const render = () => {
-        let htmlString = ""; // na początku pusty ciąg znaków//
+        let htmlString = ""; 
 
         for(const task of tasks) { 
             htmlString +=  ` 
-            <li>
+            <li 
+                ${task.done ? "style = \"text-decoration: line-through\"" : ""}>
                 ${task.content}
+                <button class="js-remove">Remove item</button>
             </li> 
             `;
         }
 
         document.querySelector(".js-items").innerHTML=htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+               removeItem(index);
+            })
+        });
+        
     }
 
+    const addNewItem = (newItemContent) => {
+
+    tasks.push({
+        content: newItemContent
+    })
+    render(); 
+}
+const removeItem = (index) => {
+    tasks.splice(index, 1);
+    render();
+}
 
     const init = () => {
         render();
 
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit)
+   
 };
 
 init ();
